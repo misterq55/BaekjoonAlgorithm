ae53e55f41f1fd603caeb26ad1ce180cce30054a
규 서문 <illsi@naver.com>
2025-03-25 22:22:42 +0900

    https://www.acmicpc.net/problem/7576 토마토

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,53 +1,105 @@
-﻿#include <algorithm>
-#include <iostream>
+﻿#include <iostream>
+#include <queue>
 #include <vector>
 using namespace std;
 
+bool zeroCheck(const int& m, const int& n, const vector<vector<int>>& tomatoField)
+{
+    bool isZero = true;
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            if (tomatoField[i][j] == 0)
+            {
+                isZero = false;
+                break;
+            }
+        }
+    }
+
+    return isZero;
+}
+
 int main()
 {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-	int n = 0;
+    int n = 0, m = 0;
 
-	cin >> n;
-	vector<int> nums(n);
-	
-	for (int i = 0; i < n; ++i)
-	{
-		cin >> nums[i];
-	}
+    cin >> n >> m;
+    cin.ignore();
 
-	sort(nums.begin(), nums.end());
-	
-	int left = 0, right = n - 1;
-	int answerLeft = left, answerRight = right;
-	int minValue = 2000000000;
-	
-	while (left < right)
-	{
-		int sum = nums[left] + nums[right];
-		int value = abs(sum);
+    vector<vector<int>> tomatoField(m + 2, vector<int>(n + 2, -1));
+    vector<vector<int>> maxDay(m + 2, vector<int>(n + 2, -1));
+        
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            cin >> tomatoField[i][j];
+        }
+    }
 
-		if (value <= minValue)
-		{
-			minValue = value;
-			answerLeft = left;
-			answerRight = right;	
-		}
+    if (zeroCheck(m, n, tomatoField))
+    {
+        cout << 0 << "\n";
+        return 0;
+    }
 
-		if (sum > 0)
-		{
-			right--;
-		}
-		else
-		{
-			left++;
-		}
-	}
+    vector<pair<int, int>> direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    queue<pair<int, int>> queue;
 
-	cout << nums[answerLeft] << " " << nums[answerRight] << "\n";
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            if (tomatoField[i][j] == 1)
+            {
+                queue.push({i, j});
+                maxDay[i][j] = 0;
+            }
+        }
+    }
+    
+    while (!queue.empty())
+    {
+        pair<int, int> currentPos = queue.front();
+        queue.pop();
+        
+        for (const auto &dir : direction)
+        {
+            int i = currentPos.first + dir.first;
+            int j = currentPos.second + dir.second;
 
-	return 0;
+            if (tomatoField[i][j] == 0)
+            {
+                queue.push({i, j});
+                tomatoField[i][j] = 1;
+
+                maxDay[i][j] = maxDay[currentPos.first][currentPos.second] + 1;
+            }
+        }
+    }
+
+    if (!zeroCheck(m, n, tomatoField))
+    {
+        cout << -1 << "\n";
+        return 0;
+    }
+
+    int answer = -1;
+    for (int i = 1; i < m + 1; ++i)
+    {
+        for (int j = 1; j < n + 1; ++j)
+        {
+            answer = max(maxDay[i][j] , answer);
+        }
+    }
+
+    cout << answer << "\n";
+    
+    return 0;
 }
