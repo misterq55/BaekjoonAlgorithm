38e54cfe62ff126909af5f92fc563f331724fc04
규 서문 <illsi@naver.com>
2025-04-16 16:10:00 +0900

    https://www.acmicpc.net/problem/12100 2048 (Easy) 푸는중

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -4,103 +4,52 @@
 #include <queue>
 using namespace std;
 
+enum E_MoveDir
+{
+    Up,
+    Down,
+    Left,
+    Right,
+};
+
+void move(vector<vector<int>> field, const int n, const E_MoveDir dir)
+{
+    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+    vector<pair<int, int>> startPos = {{n, 1}, {1, 0}, {0, n}, {0, 1}};
+
+    int count = n;
+
+    const pair<int, int> start = startPos[dir];
+    
+    for (int i = 1; i <= n; ++i)
+    {
+        vector<int> line;
+        for (int j = 1; j <= n; ++j)
+        {
+            line.push_back(field[i][j]);
+        }
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, k, l;
-    cin >> n >> k;
+    int n;
+    cin >> n;
 
     vector<vector<int>> field(n + 2, vector<int>(n + 2, -1));
-    vector<pair<int, int>> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n; ++j)
         {
-            field[i][j] = 0;
-        }
+            cin >> field[i][j];
+        }   
     }
 
-    for (int i = 1; i <= k; ++i)
-    {
-        int row, col;
-        cin >> row >> col;
-        field[row][col] = 2;
-    }
-
-    cin >> l;
-
-    int curDirIndex = 0;
-    queue<pair<int, char>> commandQ;
-    for (int i = 1; i <= l; ++i)
-    {
-        int x;
-        char c;
-        cin >> x >> c;
-        commandQ.push({x, c});
-    }
-
-    // head start
-    queue<pair<int, int>> q;
-    q.push({1, 1});
-    field[q.back().first][q.back().second] = 1;
-    
-    // move
-    bool gameOver = false;
-    int count = 0;
-
-    while (!gameOver)
-    {
-        for (int time = 1; ; ++time)
-        {
-            count++;
-            const pair<int, int>& direction = directions[curDirIndex];
-            const pair<int, int> snakeHead = q.back();
-            const pair<int, int> newPos = {snakeHead.first + direction.first, snakeHead.second + direction.second};
-
-            if (field[newPos.first][newPos.second] == 0)
-            {
-                field[newPos.first][newPos.second] = 1;
-                q.push(newPos);
-                
-                const pair<int, int> snakeTail = q.front();
-                field[snakeTail.first][snakeTail.second] = 0;
-                q.pop();
-            }
-            else if (field[newPos.first][newPos.second] == 2)
-            {
-                field[newPos.first][newPos.second] = 1;
-                q.push(newPos);
-            }
-            else if (field[snakeHead.first + direction.first][snakeHead.second + direction.second] == 1
-                || field[snakeHead.first + direction.first][snakeHead.second + direction.second] == -1)
-            {
-                gameOver = true;
-                break;
-            }
-
-            if (!commandQ.empty() && commandQ.front().first == time)
-            {
-                char dirCommand = commandQ.front().second;
-                if (dirCommand == 'D')
-                    curDirIndex = (curDirIndex + 1 + 4) % 4;
-                else if (dirCommand == 'L')
-                    curDirIndex = (curDirIndex -1 + 4) % 4;
-
-                commandQ.pop();
-            }
-        }
-
-        if (gameOver)
-        {
-            break;
-        }
-    }
-
-    cout << count << "\n";
+    move(field, n, E_MoveDir::Up);
     
     return 0;
 }
