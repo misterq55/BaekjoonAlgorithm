e52d1f61b9327e647b1a9f2994215dc815765808
규 서문 <illsi@naver.com>
2025-04-18 17:53:22 +0900

    https://www.acmicpc.net/problem/20056 마법사 상어와 파이어볼

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,113 +1,60 @@
 ﻿#include <iostream>
+#include <map>
 #include <vector>
 using namespace std;
 
-class Robot
+class FireBall
 {
 public:
-    Robot() {}
-    ~Robot() {}
+    FireBall() {}
+    FireBall(const int n, const int r, const int c, const int m, const int s, const int d)
+        : MapSize(n), Pos({r, c}), Mass(m), Speed(s), DirIndex(d)
+    {}
+    ~FireBall() {}
 
 public:
-    void SetUpInitialInfo(const int r, const int c, const int d)
+    void Move()
     {
-        Pos.first = r;
-        Pos.second = c;
-        DirIndex = d;
+        const pair<int, int> dir = Directions[DirIndex];
+        Pos.second = (Pos.second + dir.second * (Speed % MapSize) + MapSize) % MapSize;
+        Pos.first = (Pos.first + dir.first * (Speed % MapSize) + MapSize) % MapSize;
+    }
+
+    pair<int, int> GetPos() const
+    {
+        return Pos;
+    }
+
+    FireBall& operator+=(const FireBall& b)
+    {
+        Mass += b.Mass;
+        Speed += b.Speed;
+
+        return *this;
+    }
+
+    int GetMass() const
+    {
+        return Mass;
+    }
+
+    int GetSpeed() const
+    {
+        return Speed;
     }
     
-    void SetUpRoomInfo(const int n, const int m, vector<vector<int>>&& room)
+    int GetDirIndex() const
     {
-        Row = n;
-        Col = m;
-        Room = room;
-    }
-
-    void StartClean()
-    {
-        while (true)
-        {
-            const int y = Pos.first;
-            const int x = Pos.second;
-
-            // 1
-            if (Room[y][x] == 0)
-            {
-                Room[y][x] = 2;
-                CleanCount++;
-            }
-        
-            bool bisClean = true;
-            for (int i = 0; i < 4; ++i)
-            {
-                const pair<int, int> dir = GetDirection(i);
-                const int ny = y + dir.first;
-                const int nx = x + dir.second;
-                if (Room[ny][nx] == 0)
-                {
-                    bisClean = false;
-                }
-            }
-        
-            // 2
-            if (bisClean)
-            {
-                const pair<int, int> dir = GetDirection(DirIndex);
-                const int ny = y - dir.first;
-                const int nx = x - dir.second;
-                if (Room[ny][nx] != 1)
-                {
-                    Pos.first = ny;
-                    Pos.second = nx;
-                    continue;
-                }
-                else
-                {
-                    break;
-                }
-            }
-        
-            // 3
-            else
-            {
-                ChangeDirection();
-                const pair<int, int> dir = GetDirection(DirIndex);
-                const int ny = y + dir.first;
-                const int nx = x + dir.second;
-                if (Room[ny][nx] == 0)
-                {
-                    Pos.first = ny;
-                    Pos.second = nx;
-                    continue;
-                }
-            }
-        }
-    }
-
-    int GetCleanCount()
-    {
-        return CleanCount;
-    }
-
-private:
-    void ChangeDirection()
-    {
-        DirIndex = (DirIndex - 1 + 4) % 4; 
-    }
-
-    pair<int, int> GetDirection(const int index)
-    {
-        return Directons[index];
+        return DirIndex;
     }
     
 private:
-    pair<int, int> Pos;
+    pair<int, int> Pos = {0, 0};
+    int MapSize = 0;
+    int Mass = 0;
     int DirIndex = 0;
-    vector<pair<int, int>> Directons = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    vector<vector<int>> Room;
-    int Row;
-    int Col;
-    int CleanCount = 0;
+    int Speed = 0;
+    vector<pair<int, int>> Directions = {{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
 };
 
 int main()
@@ -116,28 +63,87 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    Robot robot;
-    
-    int n, m;
-    cin >> n >> m;
-    
-    vector<vector<int>> room(vector<vector<int>> (n, vector<int>(m)));
+    int n, m, k;
+    cin >> n >> m >> k;
 
-    int r, c, d;
-    cin >> r >> c >> d;
-    for (int i = 0; i < n; ++i)
+    vector<vector<int>> field(n, vector<int>(n));
+    vector<FireBall> fireballs;
+    
+    for (int i = 0; i < m; ++i)
     {
-        for (int j = 0; j < m; ++j)
-        {
-            cin >> room[i][j];
-        }
+        int r, c, ms, d, s;
+        cin >> r >> c >> ms >> s >> d;
+
+        fireballs.emplace_back(n, r, c, ms, s, d);
     }
 
-    robot.SetUpInitialInfo(r, c, d);
-    robot.SetUpRoomInfo(n, m, move(room));
+    for (int i = 0; i < k; ++i)
+    {
+        // 움직임
+        map<pair<int, int>, vector<int>> collisionMap;
+        const int size = static_cast<int>(fireballs.size());
+        for (int j = 0; j < size; ++j)
+        {
+            fireballs[j].Move();
+            const pair<int, int> pos = fireballs[j].GetPos();
+            collisionMap[pos].emplace_back(j);
+        }
 
-    robot.StartClean();
-    cout << robot.GetCleanCount() << "\n";
-    
+        vector<FireBall> newFireballs;
+        // 충돌 확인 후 융합
+        for (auto collision : collisionMap)
+        {
+            pair<int, int> pos = collision.first;
+            vector<int> collisionIndice = collision.second;
+            const int collisionSize = static_cast<int>(collisionIndice.size());
+            if (collisionSize == 0)
+            {
+                continue;
+            }
+            else if (collisionSize == 1)
+            {
+                newFireballs.emplace_back(fireballs[collisionIndice[0]]);
+            }
+            else
+            {
+                bool bDirCheckFalg = true;
+                for (int j = 1; j < collisionSize; ++j)
+                {
+                    fireballs[collisionIndice[0]] += fireballs[collisionIndice[j]];
+                    if (fireballs[collisionIndice[j]].GetDirIndex() % 2 != fireballs[collisionIndice[j - 1]].GetDirIndex() % 2)
+                    {
+                        bDirCheckFalg = false;
+                    }
+                }
+
+                // 융합된 파이어볼 분할
+                int newR = pos.first;
+                int newC = pos.second;
+                int newMass = fireballs[collisionIndice[0]].GetMass() / 5;
+                int newSpeed = fireballs[collisionIndice[0]].GetSpeed() / collisionSize;
+                int newDirIndex = bDirCheckFalg ? 0 : 1;
+                for (int j = 0; j < 4; ++j)
+                {
+                    if (newMass != 0)
+                    {
+                        newFireballs.emplace_back(n, newR,newC,newMass,newSpeed,j * 2 + newDirIndex);
+                    }
+                }
+            }
+        }
+
+        collisionMap.clear();
+        fireballs = move(newFireballs);
+    }
+
+    const int size = static_cast<int>(fireballs.size());
+    for (int i = 1; i < size; ++i)
+    {
+        fireballs[0] += fireballs[i];
+    }
+
+    const int ans = fireballs.empty() ? 0 : fireballs[0].GetMass();
+    cout << ans << "\n";
+
     return 0;
 }
