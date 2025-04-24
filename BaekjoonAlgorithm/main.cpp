b20974a4ac37d13ce10b64b2a9b87dfc35684919
규 서문 <illsi@naver.com>
2025-04-24 22:45:55 +0900

    https://www.acmicpc.net/problem/15681 트리와 쿼리

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,22 +1,37 @@
 ﻿#include <iostream>
-#include <queue>
 #include <vector>
 using namespace std;
 
-void dfs(const vector<pair<int, pair<int, pair<int, int>>>>& tree, vector<vector<int>>& widthVec, int& counter, const int n, const int index)
+void generateRootedTree(vector<vector<int>>& adj, vector<vector<int>>& tree, vector<bool>& visited, const int u)
 {
-    if (index == -1)
+    visited[u] = true;
+
+    for (int v : adj[u])
+    {
+        if (!visited[v])
+        {
+            tree[u].push_back(v);
+            generateRootedTree(adj, tree, visited, v);
+        }
+    }
+}
+
+void generateDP(vector<vector<int>>& tree, vector<int>& dp, const int r)
+{
+    dp[r] = 1;
+
+    vector<int>& children = tree[r];
+
+    if (children.empty())
     {
         return;
     }
-
-    const int depth = tree[index].first;
-    const int left = tree[index].second.second.first;
-    const int right = tree[index].second.second.second;
     
-    dfs(tree, widthVec, counter, n, left);
-    widthVec[depth].push_back(++counter);
-    dfs(tree, widthVec, counter, n, right);
+    for (int child : children)
+    {
+        generateDP(tree, dp, child);
+        dp[r] += dp[child];
+    }
 }
 
 int main()
@@ -25,88 +40,33 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    vector<pair<int, pair<int, pair<int, int>>>> tree(n + 1);
+    int n, r, q;
+    cin >> n >> r >> q;
+
+    vector<vector<int>> adj(n + 1);
+    vector<vector<int>> tree(n + 1);
+    vector<int> dp(n + 1, 0);
+    vector<bool> visited(n + 1, false);
     
-    for (int i = 1; i <= n; ++i)
+    for (int i = 1; i <= n - 1; ++i)
     {
-        int node, left, right;
-        cin >> node >> left >> right;
-        
-        tree[node].second.second.first = left;
-        tree[node].second.second.second = right;
-        
-        if (left != -1)
-        {
-            tree[left].second.first = node;
-        }
-        
-        if (right != -1)
-        {
-            tree[right].second.first = node;
-        }
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    int root = 0;
-    for (int i = 1; i <= n; ++i)
+    generateRootedTree(adj, tree, visited, r);
+    generateDP(tree, dp, r);
+
+    for (int i = 1; i <= q; ++i)
     {
-        if (tree[i].second.first == 0)
-        {
-            root = i;
-            break;
-        }
+        int u;
+        cin >> u;
+
+        cout << dp[u] << "\n";
     }
-
-    int maxDepth = 0;
-    queue<int> q;
-    tree[root].first = 1;
-    q.push(root);
-
-    while (!q.empty())
-    {
-        const int curr = q.front();
-        q.pop();
-
-        const int depth = tree[curr].first;
-        const int left = tree[curr].second.second.first;
-        const int right = tree[curr].second.second.second;
-
-        maxDepth = max(maxDepth, depth);
-        
-        if (left != -1)
-        {
-            tree[left].first = depth + 1;
-            q.push(left);
-        }
-
-        if (right != -1)
-        {
-            tree[right].first = depth + 1;
-            q.push(right);
-        }
-    }
-
-    vector<vector<int>> widthVec(maxDepth + 1);
-    int counter = 0;
-    dfs(tree, widthVec, counter, n, root);
-
-    pair<int, int> result;
-    for (int i = 1; i <= maxDepth; ++i)
-    {
-        if (widthVec[i].empty())
-        {
-            continue;
-        }
-        
-        if (result.second < widthVec[i].back() - widthVec[i].front() + 1)
-        {
-            result.first = i;
-            result.second = widthVec[i].back() - widthVec[i].front() + 1;
-        }
-    }
-
-    cout << result.first << ' ' << result.second << "\n";
     
     return 0;
 }
