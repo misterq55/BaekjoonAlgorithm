f9fa937f8d0e2714203007beaac5d907d0d59951
규 서문 <illsi@naver.com>
2025-04-24 23:50:32 +0900

    https://www.acmicpc.net/problem/14888 연산자 끼워넣기

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,36 +1,64 @@
 ﻿#include <iostream>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
-void generateRootedTree(vector<vector<int>>& adj, vector<vector<int>>& tree, vector<bool>& visited, const int u)
+void backTracking(vector<long long> nums, vector<pair<char, int>>& ops, long long& maxNum, long long& minNum)
 {
-    visited[u] = true;
-
-    for (int v : adj[u])
+    if (nums.size() == 1)
     {
-        if (!visited[v])
-        {
-            tree[u].push_back(v);
-            generateRootedTree(adj, tree, visited, v);
-        }
-    }
-}
-
-void generateDP(vector<vector<int>>& tree, vector<int>& dp, const int r)
-{
-    dp[r] = 1;
-
-    vector<int>& children = tree[r];
-
-    if (children.empty())
-    {
+        maxNum = max(maxNum, nums[0]);
+        minNum = min(minNum, nums[0]);
+        
         return;
     }
-    
-    for (int child : children)
+
+    for (pair<char, int>& op : ops)
     {
-        generateDP(tree, dp, child);
-        dp[r] += dp[child];
+        vector<long long> newNums;
+        if (op.second <= 0)
+        {
+            continue;
+        }
+        
+        long long newNum = 0;
+        switch (op.first)
+        {
+            case '+':
+                newNum = nums[0] + nums[1];
+                break;
+            case '-':
+                newNum = nums[0] - nums[1];
+                break;
+            case '*':
+                newNum = nums[0] * nums[1];
+                break;
+            case '/':
+                if (nums[0] < 0)
+                {
+                    newNum = -(-nums[0] / nums[1]);
+                }
+                else
+                {
+                    newNum = nums[0] / nums[1];
+                }
+                break;
+            default:
+                break;
+        }
+
+        op.second--;
+
+        newNums.push_back(newNum);
+
+        const int size = static_cast<int>(nums.size());
+        for (int i = 2; i < size; ++i)
+        {
+            newNums.push_back(nums[i]);
+        }
+
+        backTracking(newNums, ops, maxNum, minNum);
+        op.second++;
     }
 }
 
@@ -40,33 +68,30 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, r, q;
-    cin >> n >> r >> q;
+    int n;
+    cin >> n;
 
-    vector<vector<int>> adj(n + 1);
-    vector<vector<int>> tree(n + 1);
-    vector<int> dp(n + 1, 0);
-    vector<bool> visited(n + 1, false);
+    vector<long long> nums(n);
+    vector<pair<char, int>> ops {{'+', 0}, {'-', 0}, {'*', 0}, {'/', 0}};
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> nums[i];
+    }
+
+    for (pair<char, int>& op : ops)
+    {
+        int count;
+        cin >> count;
+        op.second = count;
+    }
+
+    long long maxNum = -1e9;
+    long long minNum = 1e9;
+
+    backTracking(nums, ops, maxNum, minNum);
     
-    for (int i = 1; i <= n - 1; ++i)
-    {
-        int u, v;
-        cin >> u >> v;
-
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-
-    generateRootedTree(adj, tree, visited, r);
-    generateDP(tree, dp, r);
-
-    for (int i = 1; i <= q; ++i)
-    {
-        int u;
-        cin >> u;
-
-        cout << dp[u] << "\n";
-    }
+    cout << maxNum << "\n";
+    cout << minNum << "\n";
     
     return 0;
 }
