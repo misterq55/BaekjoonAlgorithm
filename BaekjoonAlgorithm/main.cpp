b43e372f269a98f29daa92e72eb9432575a7e4b2
규 서문 <illsi@naver.com>
2025-05-01 12:05:46 +0900

    https://www.acmicpc.net/problem/3613 Java vs C++

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -2,37 +2,81 @@
 #include <vector>
 using namespace std;
 
-void printResult(const vector<int>& result)
+bool isValid(const string& str)
 {
-    for (const auto& num : result)
+    const int size = static_cast<int>(str.size());
+    if (size == 0)
     {
-        cout << num << " ";
+        return false;
     }
 
-    cout << "\n";
-}
-
-void backTracking(const vector<int>& nums, vector<int>& result, const int size, const int m, const int level, const int num)
-{
-    if (level == m - 1)
+    if (str[0] >= 'A' && str[0] <= 'Z' || str[0] == '_')
     {
-        printResult(result);
-        return;
+        return false;
     }
-    
-    const int nextLevel = level + 1;
-    
+
+    const int lastIndex = size - 1;
+    if (str[lastIndex] == '_')
+    {
+        return false;
+    }
+
+    int underBarCount = 0;
+    int upperCaseCount = 0;
+
     for (int i = 0; i < size; ++i)
     {
-        if (nums[i] < num)
+        if (str[i] >= 'A' && str[i] <= 'Z')
         {
-            continue;
+            ++upperCaseCount;
         }
-        
-        result.emplace_back(nums[i]);
-        backTracking(nums, result, size, m, nextLevel, nums[i]);
-        result.pop_back();
+
+        if (str[i] == '_')
+        {
+            if (str[i + 1] == '_')
+            {
+                return false;
+            }
+            
+            ++underBarCount;
+        }
     }
+
+    if (underBarCount > 0 && upperCaseCount > 0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+bool isParsingPoint(const bool isJava, const char ch)
+{
+    if (isJava)
+    {
+        return ch == '_';
+    }
+
+    return ch >= 'A' && ch <= 'Z';
+}
+
+void correctParsedStr(const bool isJava, string& parsedStr)
+{
+    if (isJava)
+    {
+        parsedStr.erase(0, 1);
+        parsedStr[0] -= ('a' - 'A');
+    }
+    else
+    {
+        parsedStr[0] += ('a' - 'A');
+        parsedStr = "_" + parsedStr;
+    }
+}
+
+void parsingString(const string str, vector<string>& parsedStr, const int offset, const int size)
+{
+    parsedStr.push_back(str.substr(offset, size));
 }
 
 int main()
@@ -41,18 +85,48 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, m;
-    cin >> n >> m;
+    string s;
+    cin >> s;
 
-    vector<int> nums;
-    vector<int> result;
-
-    for (int i = 1; i <= n; ++i)
+    if (!isValid(s))
     {
-        nums.push_back(i);
+        cout << "Error!" << "\n";
+        return 0;
     }
 
-    backTracking(nums, result, n, m, -1, 0);
+    const bool bisJava = s.find("_") != string::npos;
 
+    vector<string> parsedStr;
+
+    const int size = static_cast<int>(s.length());
+    int prevIndex = 0;
+    for (int i = 0; i < size; ++i)
+    {
+        const char ch = s[i];
+        
+        if (isParsingPoint(bisJava, ch))
+        {
+            parsingString(s, parsedStr, prevIndex, i - prevIndex);
+            prevIndex = i;
+        }
+    }
+
+    parsingString(s, parsedStr, prevIndex, size - prevIndex);
+
+    const int parsedSize = static_cast<int>(parsedStr.size());
+
+    for (int i = 1; i < parsedSize; ++i)
+    {
+        correctParsedStr(bisJava, parsedStr[i]);
+    }
+
+    string ans;
+    for (int i = 0; i < parsedSize; ++i)
+    {
+        ans += parsedStr[i];
+    }
+
+    cout << ans << "\n";
+    
     return 0;
 }
