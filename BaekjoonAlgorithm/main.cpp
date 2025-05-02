fb0c1961227e21bf473a35eb24868a8b44503a47
규 서문 <illsi@naver.com>
2025-05-02 19:28:33 +0900

    https://www.acmicpc.net/problem/1463 1로 만들기

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -13,28 +13,24 @@ int main()
     int n;
     cin >> n;
 
-    vector<int> r(n + 1, 0);
-    vector<int> g(n + 1, 0);
-    vector<int> b(n + 1, 0);
-    vector<vector<int>> dp(n + 1, vector<int>(3 + 1, 0));
+    vector<int> dp(n + 1, 0);
 
-    for (int i = 1; i <= n; ++i)
+    for (int i = 2; i <= n; ++i)
     {
-        cin >> r[i] >> g[i] >> b[i];
-    }
-    
-    dp[1][1] = r[1];
-    dp[1][2] = g[1];
-    dp[1][3] = b[1];
-    
-    for (int i = 2; i <= n; i++)
-    {
-        dp[i][1] = r[i] + min(dp[i - 1][2], dp[i - 1][3]);
-        dp[i][2] = g[i] + min(dp[i - 1][1], dp[i - 1][3]);
-        dp[i][3] = b[i] + min(dp[i - 1][1], dp[i - 1][2]);
+        dp[i] = dp[i - 1] + 1;
+
+        if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+
+        if (i % 3 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
     }
 
-    cout << min(min(dp[n][1], dp[n][2]), dp[n][3]) << "\n";
+    cout << dp[n] << "\n";
     
     return 0;
 }
