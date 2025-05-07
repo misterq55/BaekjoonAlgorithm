ea1b54ea13b16f0d8173b3bf7ba997758b1f048e
규 서문 <illsi@naver.com>
2025-05-07 18:43:36 +0900

    https://www.acmicpc.net/problem/2798 블랙잭

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,8 +1,35 @@
-﻿#include <algorithm>
-#include <iostream>
-#include <tuple>
+﻿#include <iostream>
 #include <vector>
 using namespace std;
+
+void BackTrack(const vector<int>& cards, vector<bool>& visited, const int maxCardNum, const int maxNum, const int level, int& sum, int& ans)
+{
+    if (sum > maxNum)
+    {
+        return;
+    }
+    
+    if (level == maxCardNum)
+    {
+        ans = max(sum, ans);
+        return;
+    }
+    
+    const int size = static_cast<int>(cards.size());
+    for (int i = 0; i < size; ++i)
+    {
+        if (visited[i])
+        {
+            continue;
+        }
+        
+        visited[i] = true;
+        sum += cards[i];
+        BackTrack(cards, visited, maxCardNum, maxNum, level + 1, sum, ans);
+        sum -= cards[i];
+        visited[i] = false;
+    }
+}
 
 int main()
 {
@@ -10,27 +37,24 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
+    int n, m;
+    cin >> n >> m;
 
-    vector<int> dp(n + 1, 0);
+    vector<int> cards(n);
+    
+    int maxCardNum = 3;
 
-    for (int i = 2; i <= n; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        dp[i] = dp[i - 1] + 1;
-
-        if (i % 2 == 0)
-        {
-            dp[i] = min(dp[i], dp[i / 2] + 1);
-        }
-
-        if (i % 3 == 0)
-        {
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-        }
+        cin >> cards[i];
     }
 
-    cout << dp[n] << "\n";
+    int sum = 0;
+    int ans = 0;
+    vector<bool> visited(n , false);
+    BackTrack(cards, visited, maxCardNum, m, 0, sum, ans);
+
+    cout << ans << "\n";
     
     return 0;
 }
