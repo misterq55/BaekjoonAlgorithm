7a2560ef4aeff04964e8d513b56d10eed8481ae9
규 서문 <illsi@naver.com>
2025-05-08 22:54:52 +0900

    https://www.acmicpc.net/problem/1941 소문난 칠공주

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,29 +1,19 @@
 ﻿#include <iostream>
+#include <queue>
+#include <unordered_map>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-void PrintResult(const vector<int>& result)
-{
-    for (const auto& num : result)
-    {
-        cout << num << " ";
-    }
-
-    cout << "\n";
-}
-
-void BackTrack(const vector<int>& nums, vector<bool>& visited, vector<int>& result, const int maxLevel, const int level, const int start)
+void BackTrack(vector<vector<int>>& combination, vector<int>& indexes, vector<bool>& visited, const int maxLevel, const int level, const int start)
 {
     if (level == maxLevel)
     {
-        PrintResult(result);
-        
+        combination.push_back(indexes);
         return;
     }
 
-    const int size = static_cast<int>(nums.size());
-
-    for (int i = start; i < size; ++i)
+    for (int i = start; i < 25; ++i)
     {
         if (visited[i])
         {
@@ -31,9 +21,9 @@ void BackTrack(const vector<int>& nums, vector<bool>& visited, vector<int>& resu
         }
 
         visited[i] = true;
-        result.emplace_back(nums[i]);
-        BackTrack(nums, visited, result, maxLevel, level + 1, i + 1);
-        result.pop_back();
+        indexes.push_back(i);
+        BackTrack(combination, indexes, visited, maxLevel, level + 1, i + 1);
+        indexes.pop_back();
         visited[i] = false;
     }
 }
@@ -44,30 +34,84 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    const int maxLevel = 6;
+    const int n = 5;
+
+    vector<vector<char>> field(n + 2, vector<char>(n + 2, ' '));
+    // vector<vector<bool>> visited(n + 2, vector<bool>(n + 2, false));
+    vector<char> result;
+
+    vector<int> indexes;
+    vector<bool> visted(n * n, false);
+
+    vector<vector<int>> combination;
+    const int maxLevel = 7;
     
-    while (true)
+    for (int i = 1; i <= n; ++i)
     {
-        int n;
-        cin >> n;
-
-        if (n == 0)
+        for (int j = 1; j <= n; ++j)
         {
-            break;
+            cin >> field[i][j];
         }
-
-        vector<int> nums(n);
-        vector<int> result;
-        vector<bool> visited(n, false);
-        
-        for (int i = 0; i < n; ++i)
-        {
-            cin >> nums[i];
-        }
-        
-        BackTrack(nums, visited, result, maxLevel, 0, 0);
-        cout << "\n";
     }
+
+    int count = 0;
+
+    vector<pair<int, int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    
+    BackTrack(combination, indexes, visted, maxLevel, 0, 0);
+    for (const auto& v : combination)
+    {
+        unordered_map<int, bool> indexVisited;
+
+        queue<int> q;
+        q.push(v[0]);
+        indexVisited[v[0]] = true;
+
+        int sCount = 0;
+        while (!q.empty())
+        {
+            const int curr = q.front();
+            q.pop();
+
+            const int y = curr / n + 1;
+            const int x = curr % n + 1;
+
+            if (field[y][x] == 'S')
+            {
+                sCount++;
+            }
+
+            for (const auto& direction : directions)
+            {
+                const int ny = y + direction.first;
+                const int nx = x + direction.second;
+
+                if (field[ny][nx] == ' ')
+                {
+                    continue;
+                }
+                
+                const int adj = (ny - 1) * n + nx - 1;
+                if (find(v.begin(), v.end(), adj) == v.end())
+                {
+                    continue;    
+                }
+                
+                if (!indexVisited[adj])
+                {
+                    indexVisited[adj] = true;
+                    q.push(adj);
+                }
+            }
+        }
+
+        if (indexVisited.size() == 7 && sCount >= 4)
+        {
+            count++;
+        }
+    }
+    
+    cout << count << "\n";
     
     return 0;
 }
