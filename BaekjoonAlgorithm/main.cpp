f981c4004dfd783dde704d7f4216b0a954a31369
규 서문 <illsi@naver.com>
2025-05-11 01:32:52 +0900

    https://www.acmicpc.net/problem/15659 연산자 끼워넣기

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -3,12 +3,81 @@
 #include <vector>
 using namespace std;
 
-void BackTrack(vector<long long> nums, unordered_map<long long, long long>& ops, long long& maxValue, long long& minValue)
+long long CalcNums(vector<long long> nums, vector<long long> opVec)
 {
-    if (nums.size() == 1)
+    int priNum = 0;
+    for (const auto& op : opVec)
     {
-        maxValue = max(nums[0], maxValue);
-        minValue = min(nums[0], minValue);
+        if (op >= 2)
+        {
+            priNum++;
+        }
+    }
+    
+    int index = 0;
+    while (priNum > 0)
+    {
+        const long long& op = opVec[index];
+        if (op >= 2)
+        {
+            priNum--;
+            if (op == 2)
+            {
+                nums[index] = nums[index] * nums[index + 1];
+            }
+            else if (op == 3)
+            {
+                if (nums[index] < 0)
+                {
+                    nums[index] = -(-nums[index] / nums[index + 1]);
+                }
+                else
+                {
+                    nums[index] = nums[index] / nums[index + 1];   
+                }
+            }
+            
+            nums.erase(nums.begin() + index + 1);
+            opVec.erase(opVec.begin() + index);
+        }
+        else
+        {
+            index++;
+        }
+
+        if (index >= static_cast<int>(opVec.size()))
+        {
+            break;
+        }
+    }
+
+    index = 0;
+    
+    while (!opVec.empty())
+    {
+        const long long& op = opVec[index];
+        if (op == 0)
+        {
+            nums[index] = nums[index] + nums[index + 1];
+        }
+        else if (op == 1)
+        {
+            nums[index] = nums[index] - nums[index + 1];
+        }
+            
+        nums.erase(nums.begin() + index + 1);
+        opVec.erase(opVec.begin() + index);
+    }
+    
+    return nums[0];
+}
+
+void BackTrack(vector<long long>& nums, vector<long long>& opVec, unordered_map<long long, long long>& ops, long long& maxValue, long long& minValue)
+{
+    if (nums.size() == opVec.size() + 1)
+    {
+        maxValue = max(CalcNums(nums, opVec), maxValue);
+        minValue = min(CalcNums(nums, opVec), minValue);
 
         return;
     }
@@ -22,44 +91,11 @@ void BackTrack(vector<long long> nums, unordered_map<long long, long long>& ops,
             continue;
         }
 
-        long long newNum = 0;
-        switch (i)
-        {
-        case 0:
-            newNum = nums[0] + nums[1];
-            break;
-        case 1:
-            newNum = nums[0] - nums[1];
-            break;
-        case 2:
-            newNum = nums[0] * nums[1];
-            break;
-        case 3:
-            if (nums[0] < 0)
-            {
-                newNum = (-(-nums[0]) / nums[1]);   
-            }
-            else
-            {
-                newNum = nums[0] / nums[1];
-            }
-            break;
-        default:
-            break;
-        }
-
-        vector<long long> newNums;
-        newNums.push_back(newNum);
-
-        const long long numSize = static_cast<long long>(nums.size());
-        for (long long j = 2; j < numSize; ++j)
-        {
-            newNums.push_back(nums[j]);
-        }
-        
+        opVec.push_back(i);
         op--;
-        BackTrack(newNums, ops, maxValue, minValue);
+        BackTrack(nums, opVec, ops, maxValue, minValue);
         op++;
+        opVec.pop_back();
     }
 }
 
@@ -85,7 +121,9 @@ int main()
     }
 
     long long maxValue = -1e9, minValue = 1e9;
-    BackTrack(nums, ops, maxValue, minValue);
+    vector<long long> opVec;
+    
+    BackTrack(nums, opVec, ops, maxValue, minValue);
 
     cout << maxValue << "\n";
     cout << minValue << "\n";
