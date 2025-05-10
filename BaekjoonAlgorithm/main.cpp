bbbebe5d4a5153d480c3b92dd4390f1e48e6b53e
규 서문 <illsi@naver.com>
2025-05-11 00:15:44 +0900

    https://www.acmicpc.net/problem/14888 연산자 끼워넣기

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,28 +1,65 @@
 ﻿#include <iostream>
-#include <string>
+#include <unordered_map>
 #include <vector>
 using namespace std;
 
-void BackTrack(vector<string>& equations, string& equation, const int targetNum, int& sum)
+void BackTrack(vector<long long> nums, unordered_map<long long, long long>& ops, long long& maxValue, long long& minValue)
 {
-    if (targetNum <= sum)
+    if (nums.size() == 1)
     {
-        if (targetNum == sum)
-        {
-            equations.push_back(equation);
-        }
-        
+        maxValue = max(nums[0], maxValue);
+        minValue = min(nums[0], minValue);
+
         return;
     }
 
-    for (int i = 1; i <= 3; ++i)
+    const long long opSize = static_cast<long long>(ops.size());
+    for (long long i = 0; i < opSize; ++i)
     {
-        equation += (sum == 0 ? "" : "+") + to_string(i);
-        sum += i;
-        BackTrack(equations, equation, targetNum, sum);
-        sum -= i;
-        if (sum != 0) equation.pop_back();
-        equation.pop_back();
+        long long& op = ops[i];
+        if (op == 0)
+        {
+            continue;
+        }
+
+        long long newNum = 0;
+        switch (i)
+        {
+        case 0:
+            newNum = nums[0] + nums[1];
+            break;
+        case 1:
+            newNum = nums[0] - nums[1];
+            break;
+        case 2:
+            newNum = nums[0] * nums[1];
+            break;
+        case 3:
+            if (nums[0] < 0)
+            {
+                newNum = (-(-nums[0]) / nums[1]);   
+            }
+            else
+            {
+                newNum = nums[0] / nums[1];
+            }
+            break;
+        default:
+            break;
+        }
+
+        vector<long long> newNums;
+        newNums.push_back(newNum);
+
+        const long long numSize = static_cast<long long>(nums.size());
+        for (long long j = 2; j < numSize; ++j)
+        {
+            newNums.push_back(nums[j]);
+        }
+        
+        op--;
+        BackTrack(newNums, ops, maxValue, minValue);
+        op++;
     }
 }
 
@@ -32,25 +69,26 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, k;
-    cin >> n >> k;
+    long long n;
+    cin >> n;
 
-    vector<string> equations;
-    string equation;
-
-    int sum = 0;
-
-    BackTrack(equations, equation, n, sum);
-
-    const int size = static_cast<int>(equations.size());
-    if (size < k)
+    vector<long long> nums(n);
+    for (long long i = 0; i < n; ++i)
     {
-        cout << -1 << "\n";
+        cin >> nums[i];
     }
-    else
+
+    unordered_map<long long, long long> ops = {{0, 0}, {1, 0}, {2, 0}, {3, 0},};
+    for (long long i = 0; i < 4; ++i)
     {
-        cout << equations[k - 1] << "\n";
+        cin >> ops[i];
     }
-    
+
+    long long maxValue = -1e9, minValue = 1e9;
+    BackTrack(nums, ops, maxValue, minValue);
+
+    cout << maxValue << "\n";
+    cout << minValue << "\n";
+
     return 0;
 }
