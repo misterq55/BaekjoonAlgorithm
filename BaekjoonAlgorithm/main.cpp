09b814498ff9566c65f693da5dcd133e1198d064
규 서문 <illsi@naver.com>
2025-05-12 00:59:30 +0900

    https://www.acmicpc.net/problem/13913 숨바꼭질 4

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,7 +1,6 @@
-﻿#include <deque>
+﻿#include <queue>
 #include <iostream>
-#include <deque>
-#include <unordered_map>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -22,21 +21,22 @@ int main()
     if (n == k)
     {
         cout << 0 << "\n";
+        cout << n << "\n";
         return 0;
     }
 
-    vector<int> distance(100001, 1e9);
-    vector<int> additionalSecond = {1, 1, 0};
+    vector<int> distance(100001, 0);
+    vector<int> prevNum(100001, 0);
     
-    deque<int> dq;
+    queue<int> q;
     
-    dq.push_back(n);
+    q.push(n);
     distance[n] = 0;
     
-    while (!dq.empty())
+    while (!q.empty())
     {
-        const int curr = dq.front();
-        dq.pop_front();
+        const int curr = q.front();
+        q.pop();
 
         vector<int> nums{curr - 1, curr + 1, curr * 2};
         for (int i = 0; i < 3; ++i)
@@ -44,17 +44,37 @@ int main()
             const int& num = nums[i];
             if (IsInRange(num))
             {
-                const int& additional = additionalSecond[i];
-                if (distance[num] > distance[curr] + additional)
+                if (!distance[num])
                 {
-                    distance[num] = distance[curr] + additional;
-                    additional == 0 ? dq.push_front(num) : dq.push_back(num);
+                    distance[num] = distance[curr] + 1;
+                    prevNum[num] = curr;
+                    q.push(num);
                 }
             }    
         } 
     }
 
     cout << distance[k] << "\n";
+
+    int index = k;
+
+    vector<int> result;
+    result.push_back(n);
+    
+    while (index != n)
+    {
+        result.push_back(index);
+        index = prevNum[index];
+    }
+    
+    reverse(result.begin() + 1, result.end());
+
+    for (const int& num : result)
+    {
+        cout << num << " ";
+    }
+    
+    cout << "\n";
     
     return 0;
 }
