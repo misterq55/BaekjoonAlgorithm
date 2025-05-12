731f89ae16c8dea0ee59bbc5c9ee154b44f9612b
규 서문 <illsi@naver.com>
2025-05-12 23:19:42 +0900

    https://www.acmicpc.net/problem/2206 벽 부수고 이동하기

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <queue>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -10,29 +11,66 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int numIndex = 0;
-    int val = 0;
+    int n, m;
+    cin >> n >> m;
 
-    for (int i = 1; i <= 3; ++i)
+    vector<vector<int>> field(n + 2, vector<int>(m + 2, -1));
+    vector<vector<vector<int>>> distance(n + 2, vector<vector<int>>(m + 2, vector<int>(2, -1)));
+
+    for (int i = 1; i <= n; ++i)
     {
         string s;
         cin >> s;
         
-        int tempVal = atoi(s.c_str());
-        if (tempVal > 0)
+        for (int j = 1; j <= m; ++j)
         {
-            val = tempVal;
-            numIndex = i;
+            field[i][j] = s[j - 1] - '0';
         }
     }
 
-    const int nextIndex = 4 - numIndex;
-    const int ansIndex = val + nextIndex;
+    vector<pair<int, int>> directions = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}, };
+    queue<tuple<int, int, int>> q;
 
-    if (ansIndex % 15 == 0) cout << "FizzBuzz" << "\n";
-    else if (ansIndex % 3 == 0) cout << "Fizz" << "\n";
-    else if (ansIndex % 5 == 0) cout << "Buzz" << "\n";
-    else cout << ansIndex << "\n";
+    q.push({1, 1, 0});
+    distance[1][1][0] = 1;
+
+    while (!q.empty())
+    {
+        const tuple<int, int, int> p = q.front();
+        q.pop();
+
+        const int y = get<0>(p);
+        const int x = get<1>(p);
+        const int broken = get<2>(p);
+
+        for (const auto d : directions)
+        {
+            const int ny = y + d.first;
+            const int nx = x + d.second;
+
+            if (field[ny][nx] == 0)
+            {
+                if (distance[ny][nx][broken] == -1)
+                {
+                    distance[ny][nx][broken] = distance[y][x][broken] + 1;
+                    q.push({ny, nx, broken});
+                }
+            }
+            else if (field[ny][nx] == 1 && broken == 0 && distance[ny][nx][broken] == -1)
+            {
+                distance[ny][nx][1] = distance[y][x][0] + 1;
+                q.push({ny, nx, 1});
+            }
+        }
+    }
+
+    const int dist1 = distance[n][m][0];
+    const int dist2 = distance[n][m][1];
+
+    if (dist1 == -1 && dist2 == -1) cout << -1 << "\n";
+    else if (dist1 == -1) cout << dist2 << "\n";
+    else if (dist2 == -1) cout << dist1 << "\n";
+    else cout << min(dist1, dist2) << "\n";
 
     return 0;
 }
