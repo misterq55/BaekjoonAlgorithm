b5721c78d7f5757c2c5ec1b6ed27d1d614a0af4b
규 서문 <illsi@naver.com>
2025-05-12 15:53:17 +0900

    https://www.acmicpc.net/problem/25206 너의 평점은

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -1,5 +1,47 @@
 ﻿#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
+
+class CSubject
+{
+public:
+    CSubject() {}
+    CSubject(const string& subjectName,
+        const double& mark,
+        const string& grade)
+            :SubjectName(subjectName)
+            , Mark(mark)
+            , Grade(grade)
+    {
+        TotalMark = Mark * markTable[grade];
+    }
+    
+    ~CSubject() {}
+
+public:
+    double GetTotalMark() const noexcept
+    {
+        return TotalMark;
+    }
+private:
+    unordered_map<string, double> markTable = {
+        {"A+", 4.5},
+        {"A0", 4.0},
+        {"B+", 3.5},
+        {"B0", 3.0},
+        {"C+", 2.5},
+        {"C0", 2.0},
+        {"D+", 1.5},
+        {"D0", 1.0},
+        {"F", 0},
+    };
+
+    string SubjectName;
+    double Mark = 0.0;
+    string Grade;
+    double TotalMark = 0.0;
+};
 
 int main()
 {
@@ -7,41 +49,35 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    string s, bomb, result;
-    cin >> s;
-    cin >> bomb;
+    const int n = 20;
+    vector<CSubject> subjects;
 
-    const int size = static_cast<int>(s.length());
-    const size_t boomSize = bomb.length();
-    
-    for (const char& c : s)
+    double sumMark = 0.0;
+    for (int i = 0; i < n; ++i)
     {
-        result += c;
+        string subjectName;
+        double mark;
+        string grade;
+        cin >> subjectName >> mark >> grade;
 
-        if (result.size() >= boomSize)
+        if (grade == "P")
         {
-            bool bisBomb = true;
-
-            for (int i = 0; i < boomSize; ++i)
-            {
-                if (result[result.size() - boomSize + i] != bomb[i])
-                {
-                    bisBomb = false;
-                    break;
-                }
-            }
-
-            if (bisBomb)
-            {
-                for (int i = 0; i < boomSize; ++i)
-                {
-                    result.pop_back();
-                }
-            }
+            continue;
         }
+
+        sumMark += mark;
+        
+        subjects.emplace_back(subjectName, mark, grade);
     }
 
-    cout << (result.empty() ? "FRULA" : result) << "\n";
-    
+    double sumTotalMark = 0.0;
+
+    for (const CSubject& subject : subjects)
+    {
+        sumTotalMark += subject.GetTotalMark();
+    }
+
+    cout << sumTotalMark / sumMark << "\n";
+
     return 0;
 }
