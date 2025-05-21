aafe7d7d361d0dbe6b90f1b0fc51d1af8ed0d593
규 서문 <illsi@naver.com>
2025-05-21 17:11:59 +0900

    https://www.acmicpc.net/problem/1238 파티

--- BaekjoonAlgorithm/main.cpp
+++ BaekjoonAlgorithm/main.cpp
@@ -3,84 +3,72 @@
 #include <vector>
 using namespace std;
 
+vector<int> Dijkstra(const vector<vector<pair<int, int>>>& graph, const int start)
+{
+    const int INF = static_cast<int>(1e9);
+    vector<int> distances(graph.size(), INF);
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, start});
+    distances[start] = 0;
+
+    while (!pq.empty())
+    {
+        const pair<int, int> current = pq.top();
+        pq.pop();
+
+        const int cost = current.first;
+        const int u = current.second;
+
+        if (cost > distances[u])
+        {
+            continue;
+        }
+
+        for (const pair<int, int>& node : graph[u])
+        {
+            const int v = node.second;
+            const int w = node.first;
+
+            if (distances[v] > distances[u] + w)
+            {
+                distances[v] = distances[u] + w;
+                pq.push(node);
+            }
+        }
+    }
+
+    return move(distances);
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n;
-    cin >> n;
-    
-    vector<vector<pair<int, int>>> tree(n + 1);
+    int n, m, x;
+    cin >> n >> m >> x;
 
+    vector<vector<pair<int, int>>> graph0(n + 1), graph1(n + 1);
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v, w;
+        cin >> u >> v >> w;
+        graph0[u].push_back({w, v});
+        graph1[v].push_back({w, u});
+    }
+
+    vector<int> fromX = Dijkstra(graph0, x);
+    vector<int> toX = Dijkstra(graph1, x);
+
+    int maxValue = 0;
     for (int i = 1; i <= n; ++i)
     {
-        int index;
-        cin >> index;
-        
-        while (true)
-        {
-            int node;
-            cin >> node;
-            
-            if (node == -1)
-            {
-                break;
-            }
-
-            int weight;
-            cin >> weight;
-
-            tree[index].push_back(make_pair(weight, node));
-        }
-    }
-    
-    vector<pair<int, int>> data = {{0, 1}};
-    vector<int> distances;
-    int ans = 0;
-
-    for (int i = 0; i < 2; ++i)
-    {
-        priority_queue<pair<int, int>> pq;
-        distances = move(vector<int>(n + 1, -1));
-    
-        pq.push(data[i]);
-        distances[data[i].second] = 0;
-        
-        while (!pq.empty())
-        {
-            const pair<int, int> curr = pq.top();
-            pq.pop();
-
-            for (const pair<int, int>& node : tree[curr.second])
-            {
-                if (distances[node.second] == -1)
-                {
-                    distances[node.second] = distances[curr.second] + node.first;
-                    pq.push(node);
-                }
-            }
-        }
-
-        int maxDistIndex = 0;
-        int maxDist = 0;
-        for (int j = 1; j <= n; ++j)
-        {
-            const int distance = distances[j];
-            if (distance > maxDist)
-            {
-                maxDist = distance;
-                maxDistIndex = j;
-            }
-        }
-
-        ans = maxDist;
-
-        data.push_back({0, maxDistIndex});
+        maxValue = max(maxValue, fromX[i] + toX[i]);
     }
 
-    cout << ans << "\n";
+    cout << maxValue << "\n";
     
     return 0;
 }
